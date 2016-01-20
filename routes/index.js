d5a6f89c85b555df6b8b11a4ccbe190e957a30e9
JeremyLopez <jeremyy7@gmail.com>
2016-01-20 14:11:45 -0500

    included some comments

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 var Disease = mongoose.model('Disease');
 //var Comment = mongoose.model('Comment');
-var app2 = express();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +12,6 @@ router.get('/', function(req, res, next) {
 
 router.get('/diseases', function(req, res, next) {
 	console.log("here");
-	console.log(app2.routes);
   Disease.find(function(err, diseases){
     if(err){ return next(err); }
 
@@ -20,23 +19,7 @@ router.get('/diseases', function(req, res, next) {
   });
 });
 
-
-//router.route('/deseases/:id') 
-//	.get(function(req, res, next) { 
-//		next(new Error('not implemented')); 
-//	}) 
-//	.put(function(req, res, next) { 
-//		next(new Error('not implemented')); 
-//	}) 
-//	.post(function(req, res, next) { 
-//		next(new Error('not implemented')); 
-//	}) 
-//	.delete(function(req, res, next) { 
-//		next(new Error('not implemented')); 
-//	});
-
-
-
+// Unsure if this works
 router.get("/diseases/:id", function(req, res, next) {
 	Disease.findOne({_id: req.params.id}, function(err, disease) {
 		if (err){ return next(err); }
@@ -44,6 +27,7 @@ router.get("/diseases/:id", function(req, res, next) {
 	});
 });
 
+//Definitely works
 router.post('/diseases', function(req, res, next) {
   var disease = new Disease(req.body);
 
@@ -54,37 +38,39 @@ router.post('/diseases', function(req, res, next) {
   });
 });
 
-//router.delete('/diseases', function(req, res, next) {
-//	const id = req.params.id;
+
+// Not working
+router.delete('/diseases', function(req, res, next) {
+	const id = req.params.id;
+	console.log(req);
+//	Disease.remove({ _id: req.params.id }, 
+//		function(err, diseases) {
+//			if (err) { 
+//				return next(err); 
+//			}
+//		
+//			res.json(diseases);
+//	});
+});
+
+//router.route('/diseases/:id').delete(function(req, res) {
+//	_id: req.params.id
 //	console.log(req);
-////	Disease.remove({ _id: req.params.id }, 
-////		function(err, diseases) {
-////			if (err) { 
-////				return next(err); 
-////			}
-////		
-////			res.json(diseases);
-////	});
 //});
 
-router.route('/diseases/:id').delete(function(req, res) {
-	_id: req.params.id
-	console.log(req);
-});
 
 
-
-router.param('disease', function(req, res, next, id) {
-	var query = Disease.findById(id);
-	
-	query.exec(function(err, disease) {
-		if (err) { return next(err); }
-		if (!disease) { return next(new Error('can\'t find disease')); }
-		
-		req.disease = disease;
-		return next();
-	});
-});
+//router.param('disease', function(req, res, next, id) {
+//	var query = Disease.findById(id);
+//	
+//	query.exec(function(err, disease) {
+//		if (err) { return next(err); }
+//		if (!disease) { return next(new Error('can\'t find disease')); }
+//		
+//		req.disease = disease;
+//		return next();
+//	});
+//});
 
 
 
